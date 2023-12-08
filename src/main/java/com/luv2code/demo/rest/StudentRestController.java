cd3fe04eab5d10e6a56e79655ca63a656cf32add
gauzam <gautam01428@gmail.com>
2023-12-08 21:06:08 +0530

    Added ControllerAdvice

--- src/main/java/com/luv2code/demo/rest/StudentRestController.java
+++ src/main/java/com/luv2code/demo/rest/StudentRestController.java
@@ -49,39 +49,4 @@ public class StudentRestController {
         return theStudents.get(studentId);
     }
 
-    //add an exception handler using @ExceptionHandler
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc){
-
-        // create a StudentErrorResponse
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        //setting appropriate value on this error response object
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        //return a ResponseEntity
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-
-    }
-
-    //add another exception handler to catch any other exceptions (all exceptions)
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(Exception exc){
-
-        // create a StudentErrorResponse
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        //setting appropriate value on this error response object
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
-
-        //return a ResponseEntity
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
-
-    }
-
-
 }
